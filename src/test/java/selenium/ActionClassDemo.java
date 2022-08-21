c12387e98474c936ab58aecbd246a441136ee0be
alina <alinazoldubaeva@gmail.com>
2022-08-21 12:43:42 -0700

    Initial commit from Alina's branch

--- src/test/java/selenium/ActionClassDemo.java
+++ src/test/java/selenium/ActionClassDemo.java
@@ -13,6 +13,8 @@ import org.openqa.selenium.support.Color;
 public class ActionClassDemo {
 
     public static void main(String[] args) {
+
+        System.out.println("Code change from Alina");
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
 
